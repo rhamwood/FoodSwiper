253e1d87e440110fb42da7043a093f7d1e3f7e12
rowanhamwood <rhamwood@gmail.com>
2022-04-12 22:36:47 +0200

    added Banner ad to top of screen, deleted top app bar

--- app/src/main/java/org/rowanhamwood/hungr/MainActivity.kt
+++ app/src/main/java/org/rowanhamwood/hungr/MainActivity.kt
@@ -1,7 +1,6 @@
 package org.rowanhamwood.hungr
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
@@ -9,7 +8,11 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.rowanhamwood.hungr.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +22,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        MobileAds.initialize(this) { }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val mAdView = binding.adView
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         val navView: BottomNavigationView = binding.navView
 
@@ -29,14 +38,14 @@ class MainActivity : AppCompatActivity() {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_swipe, R.id.navigation_search, R.id.navigation_recipe_list
-
-            )
-        )
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_swipe, R.id.navigation_search, R.id.navigation_recipe_list
+//
+//            )
+//        )
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         navView.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_700))
 
